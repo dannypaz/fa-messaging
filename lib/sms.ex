579459cfd8335f9e00f37f60c3d9e19d5194edfb
Kata Martin <kata@futureadvisor.com>
2017-03-30 17:28:32 -0700

    Return info about sent and tried text counts on success

--- lib/sms.ex
+++ lib/sms.ex
@@ -6,17 +6,32 @@ defmodule FutureAdvisorMessaging.Sms do
       list_empty?(numbers) ->
         {:error, "Must provide at least one (1) valid phone number."}
       true ->
-        Enum.each(numbers, fn(x) -> send_to_number(x, message) end)
-        {:ok, "Total: Sent:"}
+        sent = Enum.reduce(numbers, 0, fn(x, acc) -> send_to_number(x, message) + acc end)
+        tried = Kernel.length(numbers)
+        {:ok, {sent, tried}}
     end
   end
 
   defp send_to_number("", _message) do
-    {:error, "number cannot be blank"}
+    0
   end
 
   defp send_to_number(user_phone, message) do
     FutureAdvisorMessaging.Twilio.create(user_phone, message)
+    |> get_status_from_response
+    |> get_value_from_status
+  end
+
+  def get_status_from_response(response) do
+    response[:status]
+  end
+
+  def get_value_from_status("accepted") do
+    1
+  end
+
+  def get_value_from_status(_status) do
+    0
   end
 
   defp list_empty?(list) do
